16e764ea7bb77b9b657a349c51eba64100acf851
Andy Armor <aoarmor25@students.bmchs.orggit config --global user.name Andy>
2022-12-01 14:29:51 -0600

    done

--- lab5-1.py
+++ lab5-1.py
@@ -27,5 +27,18 @@ Write down what was printed out. What type is my_dictionary?
 Add a line of code that will print the definition of 'chair', then run the code again.
 
 Write down what happens if you use my_dictionary['kittens']? What do you think that error means?
+it gives you an error and means that it is not apart of my_dictionary
 '''
 
+my_dictionary = {
+'cat': 'a domestic feline',
+'dog': 'a domestic canine',
+'chair': 'furniture piece for sitting',
+'car': 'automobile'}
+print(my_dictionary)
+print(my_dictionary['dog'])
+print(my_dictionary.get('dog'))
+print(my_dictionary['chair'])
+print('cat' in my_dictionary)
+print('monkey' in my_dictionary)
+my_dictionary['kittens']
